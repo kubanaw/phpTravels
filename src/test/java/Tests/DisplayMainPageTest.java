780aef8d5510598575a0694e31bc366dbbd02aee
krzysztofpodlaski <krzysztof.podlaski@yahoo.pl>
2018-06-10 13:15:24 +0200

    Windows driver.

--- src/test/java/Tests/DisplayMainPageTest.java
+++ src/test/java/Tests/DisplayMainPageTest.java
@@ -1,5 +1,34 @@
 package Tests;
 
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.support.ui.Select;
+import static org.junit.Assert.assertEquals;
+
 public class DisplayMainPageTest {
+
+//    private  WebDriver driver;
+//
+//    @BeforeClass
+//    public static void pathSetup() {
+//        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
+//    }
+//
+//    @Before
+//    public void setup(){
+//        this.driver = new ChromeDriver();
+//    }
+//
+//    @After
+//    public void CloseBrowser(){
+//        this.driver.quit();
+//    }
+//    @Test
+
 
 }
