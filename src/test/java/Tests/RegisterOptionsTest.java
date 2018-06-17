1a8f1179bc6915f0cecbc16eb0ec83b22800b0d8
NWelter <nataliawelter@wp.pl>
2018-06-17 13:38:27 +0200

    add negative email field test

--- src/test/java/Tests/RegisterOptionsTest.java
+++ src/test/java/Tests/RegisterOptionsTest.java
@@ -27,7 +27,7 @@ public class RegisterOptionsTest {
 //    }
 
     @Test
-    public void whenUserFirstNameFieldIsNotFillThenUserShouldNotRegister () {
+    public void whenUserFirstNameFieldIsNotFillThenUserShouldNotRegister() {
 
         this.driver.findElement(By.name("lastname")).sendKeys("Moczymorda");
         this.driver.findElement(By.name("phone")).sendKeys("0048664111332");
@@ -50,10 +50,7 @@ public class RegisterOptionsTest {
 
         String message = this.driver.findElement(By.className("alert-danger")).getText();
         Assert.assertEquals(message, "The First name field is required.");
-        Assert.assertEquals(this.driver.getCurrentUrl(),"https://www.phptravels.net/register");
-
-
-
+        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
 
 
     }
@@ -61,6 +58,7 @@ public class RegisterOptionsTest {
     @Test
     public void whenUserLastNameFieldIsNotFillThenUserShouldNotRegister() {
         this.driver.findElement(By.name("firstname")).sendKeys("Gerwazy");
+
         this.driver.findElement(By.name("phone")).sendKeys("0048664111332");
         this.driver.findElement(By.name("email")).sendKeys("j3686875@nwytg.com");
         this.driver.findElement(By.name("password")).sendKeys("M4ki3t9!");
@@ -79,8 +77,39 @@ public class RegisterOptionsTest {
         }
         String message = this.driver.findElement(By.className("alert-danger")).getText();
         Assert.assertEquals(message, "The Last Name field is required.");
-        Assert.assertEquals(this.driver.getCurrentUrl(),"https://www.phptravels.net/register");
+        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+
+
     }
 
+    @Test
+    public void whenUserEmailFieldIsNotFillThenUserShouldNotRegister() {
+        this.driver.findElement(By.name("firstname")).sendKeys("Gerwazy");
+        this.driver.findElement(By.name("lastname")).sendKeys("Moczymorda");
+        this.driver.findElement(By.name("phone")).sendKeys("0048664111332");
+        this.driver.findElement(By.name("password")).sendKeys("M4ki3t9!");
+        this.driver.findElement(By.name("confirmpassword")).sendKeys("M4ki3t9!");
+        try {
+            Thread.sleep(1100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.driver.findElement(By.xpath("/html/body/div[4]/section/div/div/div/div/div[2]/div/form/div[9]/button")).click();
 
+        try {
+            Thread.sleep(1100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String message = this.driver.findElement(By.className("alert-danger")).getText();
+        Assert.assertEquals(message, "The Email field is required.");
+        Assert.assertEquals(this.driver.getCurrentUrl(),"https://www.phptravels.net/register");
+
+
+    }
+
+    @Test
+    public void whenUserPasswordFieldNotFillThenUserShouldNotRegister(){
+
+    }
 }
