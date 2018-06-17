a884792bbca9019a9e87f58d638effbcd889848d
NWelter <nataliawelter@wp.pl>
2018-06-17 13:29:15 +0200

    add negative last name registration, change login prerequisites

--- src/test/java/Tests/RegisterOptionsTest.java
+++ src/test/java/Tests/RegisterOptionsTest.java
@@ -18,6 +18,7 @@ public class RegisterOptionsTest {
     @Before
     public void setup() {
         this.driver = new ChromeDriver();
+        this.driver.get("https://www.phptravels.net/register");
     }
 
 //    @After
@@ -27,7 +28,7 @@ public class RegisterOptionsTest {
 
     @Test
     public void whenUserFirstNameFieldIsNotFillThenUserShouldNotRegister () {
-        this.driver.get("https://www.phptravels.net/register");
+
         this.driver.findElement(By.name("lastname")).sendKeys("Moczymorda");
         this.driver.findElement(By.name("phone")).sendKeys("0048664111332");
         this.driver.findElement(By.name("email")).sendKeys("j3686875@nwytg.com");
@@ -56,4 +57,30 @@ public class RegisterOptionsTest {
 
 
     }
+
+    @Test
+    public void whenUserLastNameFieldIsNotFillThenUserShouldNotRegister() {
+        this.driver.findElement(By.name("firstname")).sendKeys("Gerwazy");
+        this.driver.findElement(By.name("phone")).sendKeys("0048664111332");
+        this.driver.findElement(By.name("email")).sendKeys("j3686875@nwytg.com");
+        this.driver.findElement(By.name("password")).sendKeys("M4ki3t9!");
+        this.driver.findElement(By.name("confirmpassword")).sendKeys("M4ki3t9!");
+        try {
+            Thread.sleep(1100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.driver.findElement(By.xpath("/html/body/div[4]/section/div/div/div/div/div[2]/div/form/div[9]/button")).click();
+
+        try {
+            Thread.sleep(1100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String message = this.driver.findElement(By.className("alert-danger")).getText();
+        Assert.assertEquals(message, "The Last Name field is required.");
+        Assert.assertEquals(this.driver.getCurrentUrl(),"https://www.phptravels.net/register");
+    }
+
+
 }
