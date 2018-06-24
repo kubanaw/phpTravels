33ad0e4715f4142a68d1b2b0d997771eb46214d2
Ilona Kierepka <ilona.kierepka@gmail.com>
2018-06-24 12:02:45 +0200

    RegisterPage class was updated

--- src/test/java/Pages/RegisterPage.java
+++ src/test/java/Pages/RegisterPage.java
@@ -1,9 +1,16 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class RegisterPage {
+    WebDriver driver;
+
+    public RegisterPage(WebDriver driver) {
+        this.driver=driver;
+    }
+    //ELEMENTS:
     @FindBy(name = "firstname")
     WebElement firstName;
     @FindBy(name = "lastname")
@@ -18,5 +25,29 @@ public class RegisterPage {
     WebElement confirmPassword;
     @FindBy(xpath ="//*[@id=\"headersignupform\"]/div[9]/button")
     WebElement submit;
+
+    //METHODS:
+
+    public void fillFirstname(String typeName) {
+        firstName.sendKeys(typeName);
+    }
+    public void fillLastName(String typeLastName) {
+        lastName.sendKeys(typeLastName);
+    }
+    public void fillMobileNumber(String typeMobileNumber) {
+        mobileNumber.sendKeys(typeMobileNumber);
+    }
+    public void fillEmailAdress(String typeEmail) {
+        emailAdress.sendKeys(typeEmail);
+    }
+    public void fillPassword(String typePasword) {
+        password.sendKeys(typePasword);
+    }
+    public void fillConfirmPassword(String typeConfirmPassword) {
+        confirmPassword.sendKeys(typeConfirmPassword);
+    }
+    public void clickSubmit() {
+        submit.submit();
+    }
 
 }
