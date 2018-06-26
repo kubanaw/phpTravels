fd7b1726167653f2201a4a14c76752b7b02f0875
krzysztofpodlaski <krzysztof.podlaski@yahoo.pl>
2018-06-26 16:20:05 +0200

    Refactor for MainNavbarTest.java Splitting to proper tests.

--- src/test/java/Tests/DisplayMainNavbarTest.java
+++ src/test/java/Tests/DisplayMainNavbarTest.java
@@ -97,7 +97,6 @@ public class DisplayMainNavbarTest {
         String assertHotels = this.driver.findElement(By.cssSelector("li.main-lnk:nth-child(2) > a:nth-child(1)")).getText();
         Assert.assertEquals(assertHotels, "HOTELS");
         this.driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul[1]/li[2]/a")).click();
-
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -105,8 +104,31 @@ public class DisplayMainNavbarTest {
         }
         Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/hotels");
         this.driver.get("https://www.phptravels.net/");
+        //DELAY Until closing
+        try {
+            Thread.sleep(2700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //DELAY-END
 
 
+    }
+
+    @Test
+    public void GivenThahtFlightsPageExist() {
+        this.driver.get("https://www.phptravels.net/");
+        String resultFlights = this.driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul[1]/li[3]/a")).getText();
+        Assert.assertEquals(resultFlights, "FLIGHTS");
+        this.driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul[1]/li[3]/a")).click();
+        this.driver.get("https://www.phptravels.net/");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/flights");
+        this.driver.get("https://www.phptravels.net/");
         //DELAY Until closing
         try {
             Thread.sleep(2700);
