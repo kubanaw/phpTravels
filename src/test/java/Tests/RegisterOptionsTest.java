fd682c08212c1988843f479523025769b4671330
NWelter <nataliawelter@wp.pl>
2018-07-14 19:35:11 +0200

    code refactor, remove unused methods, change selectors in register page

--- src/test/java/Tests/RegisterOptionsTest.java
+++ src/test/java/Tests/RegisterOptionsTest.java
@@ -2,11 +2,8 @@ package Tests;
 
 import Pages.RegisterPage;
 import org.junit.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import util.ChromeDrvPathHelper;
 
@@ -14,11 +11,9 @@ import java.util.concurrent.TimeUnit;
 
 public class RegisterOptionsTest {
     //TODO refactor (create DDT)
-
-
     private WebDriver driver;
     private RegisterPage registerPage;
-    private WebDriverWait waitTime;
+    private WebDriverWait wait;
 
     @BeforeClass
     public static void ustawSciezke() {
@@ -29,11 +24,11 @@ public class RegisterOptionsTest {
     @Before
     public void setup() {
         this.driver = new ChromeDriver();
-        this.waitTime = new WebDriverWait(driver, 2);
+        driver.manage().window().maximize();
+        this.wait = new WebDriverWait(driver, 3);
         this.registerPage = new RegisterPage(driver);
         registerPage.goToRegisterPage();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @After
@@ -46,12 +41,11 @@ public class RegisterOptionsTest {
     @Test
     public void whenAllNecessaryFieldsAreFilledThenUserShouldRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillEmailAdress("j3776@nwytg.com");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         //is any way to make it better?
         try {
@@ -59,27 +53,27 @@ public class RegisterOptionsTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/account/");
+        //TODO add String url in Account Page class
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.phptravels.net/account/");
 
     }
 
     @Test
     public void whenAllFieldsAreFilledThenUserShouldRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillMobileNumber("440086544");
         registerPage.fillEmailAdress("j368687577116@nwytg.com");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/account/");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.phptravels.net/account/");
 
     }
 
@@ -88,12 +82,10 @@ public class RegisterOptionsTest {
     @Test
     public void whenAllFieldsAreNotFilledThenUserShouldNotRegister() {
 
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Email field is required.\nThe Password field is required.\nThe Password field is required." +
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Email field is required.\nThe Password field is required.\nThe Password field is required." +
                 "\nThe First name field is required.\nThe Last Name field is required.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
@@ -105,78 +97,71 @@ public class RegisterOptionsTest {
         registerPage.fillEmailAdress("j368687577116@nwytg.com");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The First name field is required.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+        Assert.assertEquals(registerPage.getAlertMessage(), "The First name field is required.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
     @Test
     public void whenUserLastNameFieldIsNotFillThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillMobileNumber("440086544");
         registerPage.fillEmailAdress("j368687577116@nwytg.com");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Last Name field is required.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Last Name field is required.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
     @Test
     public void whenUserEmailFieldIsNotFillThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillMobileNumber("440086544");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Email field is required.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Email field is required.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
     @Test
     public void whenUserPasswordFieldNotFillThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillMobileNumber("440086544");
         registerPage.fillEmailAdress("j368687577116@nwytg.com");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Password field is required.\nThe Password field is required.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Password field is required.\nThe Password field is required.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
@@ -185,64 +170,60 @@ public class RegisterOptionsTest {
     @Test
     public void whenEmailFieldHasInvalidValueThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillEmailAdress("j3686875776nwytg.com");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
         //TODO catch TimeoutException to logger
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Email field must contain a valid email address.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Email field must contain a valid email address.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
     @Test
     public void whenEmailFieldHasWhiteSpaceBeforeThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillEmailAdress(" j3686875776@nwytg.com");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Email field must contain a valid email address.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Email field must contain a valid email address.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
     @Test
     public void whenEmailFieldHasWhiteSpaceAfterThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillEmailAdress("j3686875776@nwytg.com ");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3t9!");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Email field must contain a valid email address.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Email field must contain a valid email address.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
@@ -251,61 +232,57 @@ public class RegisterOptionsTest {
     @Test
     public void whenPasswordHasLessThanSixCharThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillEmailAdress("j3686875776@nwytg.com");
         registerPage.fillPassword("M4ki3");
         registerPage.fillConfirmPassword("M4ki3");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "The Password field must be at least 6 characters in length.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+        Assert.assertEquals(registerPage.getAlertMessage(), "The Password field must be at least 6 characters in length.");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
     @Test
     public void whenPasswordHasSixCharThenUserShouldRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillEmailAdress("j3675776@nwytg.com");
         registerPage.fillPassword("M4ki3t");
         registerPage.fillConfirmPassword("M4ki3t");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/account/");
+        Assert.assertEquals(driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
     @Test
     public void whenConfirmPasswordIsInaccurateThenUserShouldNotRegister() {
 
-        registerPage.fillFirstname("Gerwazy");
+        registerPage.fillFirstName("Gerwazy");
         registerPage.fillLastName("Moczymorda");
         registerPage.fillEmailAdress("j3686875776@nwytg.com");
         registerPage.fillPassword("M4ki3t9!");
         registerPage.fillConfirmPassword("M4ki3");
-        Assert.assertTrue(registerPage.getSubmit().isEnabled());
         registerPage.clickSubmit();
         try {
             Thread.sleep(1100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String message = registerPage.getAlert().getText();
-        Assert.assertEquals(message, "Password not matching with confirm password.");
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://www.phptravels.net/register");
+
+        Assert.assertEquals(registerPage.getAlertMessage(), "Password not matching with confirm password.");
+        Assert.assertEquals(this.driver.getCurrentUrl(), registerPage.getUrl());
 
     }
 
