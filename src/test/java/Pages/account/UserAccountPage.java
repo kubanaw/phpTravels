81fdebe2f1465d911ec38ec199e80e5a37bfd7cd
Ilona Kierepka <ilona.kierepka@gmail.com>
2018-08-05 13:01:56 +0200

    code refactor on profile tabs test

--- src/test/java/Pages/account/UserAccountPage.java
+++ src/test/java/Pages/account/UserAccountPage.java
@@ -1,12 +1,9 @@
 package Pages.account;
 
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
@@ -127,6 +124,7 @@ public class UserAccountPage {
         for (WebElement webElement : bookingDetails) {
             details.add(webElement.getText());
         }
+        LOGGER.info("booking: "+details);
         return details;
     }
 
