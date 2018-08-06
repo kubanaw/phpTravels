12614a22ffe71805edbd81a9fe5e149a2d02f4aa
krzysztofpodlaski <krzysztof.podlaski@yahoo.pl>
2018-08-06 14:05:41 +0200

    DisplayFooterElementTest - added assertion on active/inactive block: subscriberesponse -> if email is new, Assertion will check if response is: SUBSCRIBED SUCCESSFULLY.

--- src/test/java/Tests/mainPageTest/DisplayFooterElementTest.java
+++ src/test/java/Tests/mainPageTest/DisplayFooterElementTest.java
@@ -6,12 +6,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import util.ChromeDrvPathHelper;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DisplayFooterElementTest {
     //ilona tutaj pisze
@@ -32,18 +36,26 @@ public class DisplayFooterElementTest {
 
     @After
     public void tearDown() {
+        driver.manage().timeouts().implicitlyWait(14,TimeUnit.SECONDS);
         driver.quit();
+
     }
 
     //zapis na newsletter
     @Test
     public void addEmailToNewsletterList() {
         this.driver.get("https://www.phptravels.net/");
-        this.footer.getEmailField().sendKeys("testqwerty3210@gmail.com");
+        this.footer.getEmailField().sendKeys("abactestqwerty3210@gmail.com");
         this.footer.getSubmitNewsletterButton().click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        String subscribedSuccessfullyAlert = driver.findElement(By.xpath("//*[@id=\"footer\"]/div/div[1]/div/ul/li/a/div")).getText();
+        System.out.println(subscribedSuccessfullyAlert);
+        assertEquals("SUBSCRIBED SUCCESSFULLY", subscribedSuccessfullyAlert);
+//        String subscribedSuccessfullyAlert = driver.findElement(By.xpath("//*[@id=\"footer\"]/div/div[1]/div/ul/li/a/div")).getText();
+//        System.out.println(subscribedSuccessfullyAlert);
+//        assertEquals("ALREADY SUBSCRIBED", subscribedSuccessfullyAlert);
 
-//footer.
+
 
     }
 
