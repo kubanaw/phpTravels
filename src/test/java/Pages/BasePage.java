7151e162e2fff5d59a8ca7cddebbefd6c5823d3d
NWelter <nataliawelter@wp.pl>
2018-08-21 00:26:48 +0200

    add new tests into MyProfilePageTest, change locators in NavbarHeader, move randomString to BasePage

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,11 +1,15 @@
 package Pages;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.logging.Logger;
 
 public abstract class BasePage {
         //TODO all pages inherit from basepage
 
         protected final static Logger LOGGER = Logger.getLogger(BasePage.class.getName());
+        protected  String randomString = RandomStringUtils.randomAlphabetic(7, 10);
+        protected  String randomEmail = randomString + "@nwytg.pl";
 
 
 }
--- src/test/java/Tests/userAccountTest/MyProfilePageTest.java
+++ src/test/java/Tests/userAccountTest/MyProfilePageTest.java
@@ -1,10 +1,12 @@
 package Tests.userAccountTest;
 
+import Pages.commonPages.NavbarHeader;
 import Pages.user.LoginPage;
 import Pages.user.account.MyProfilePage;
 import Pages.user.account.UserAccountPage;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,14 +14,17 @@ import util.ChromeDrvPathHelper;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MyProfilePageTest {
-    //TODO change assertions to AssertJ and loggers
 
     private WebDriver driver;
+    private WebDriverWait wait;
     private MyProfilePage myProfilePage;
     private LoginPage loginPage;
     private UserAccountPage userAccountPage;
-    private WebDriverWait wait;
+    private NavbarHeader navbarHeader;
+
 
 
     @BeforeClass
@@ -30,12 +35,13 @@ public class MyProfilePageTest {
 
     @Before
     public void setup() {
-        this.driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        this.wait = new WebDriverWait(this.driver, 2);
-        this.myProfilePage = new MyProfilePage(driver);
-        this.loginPage = new LoginPage(driver);
-        this.userAccountPage = new UserAccountPage(driver);
+        wait = new WebDriverWait(this.driver, 2);
+        myProfilePage = new MyProfilePage(driver);
+        loginPage = new LoginPage(driver);
+        userAccountPage = new UserAccountPage(driver);
+        navbarHeader = new NavbarHeader(driver);
         loginPage.loginPageOpen();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         loginPage.loginDemoUser();
@@ -48,51 +54,29 @@ public class MyProfilePageTest {
 //        this.driver.quit();
 //    }
 
-//    @Test
-//    public void allProfilesFormFieldsShouldBeDisplayed() {
-//        //TODO catch NoSuchElementException to logger or remove this test
-//            Assert.assertTrue(myProfilePage.getFirstNameInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getLastNameInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getPhoneNumberInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getEmailInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getPasswordInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getConfirmPasswordInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getFirstAddressInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getSecondAddressInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getCityInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getStateInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getZipCodeInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getCountryListInput().isDisplayed());
-//            Assert.assertTrue(myProfilePage.getSubmitUpdateProfileButton().isDisplayed());
-//    }
-//
-//    @Test
-//    public void userShouldNotBeAllowedToChangeFirstNameField() {
-//        String firstName = myProfilePage.getFirstNameInput().getAttribute("value");
-//        myProfilePage.fillFirstName("Natalia");
-//        String changeName = myProfilePage.getFirstNameInput().getAttribute("value");
-//        Assert.assertEquals(firstName, "Johny");
-//        Assert.assertNotEquals(firstName, "Natalia");
-//        Assert.assertEquals(firstName, changeName);
-//    }
-//
-//    @Test
-//    public void userShouldNotBeAllowedToChangeLastNameField() {
-//        //TODO ask is that good solution
-//        String lastName = myProfilePage.getLastNameInput().getAttribute("value");
-//        myProfilePage.fillLastName("Kowalska");
-//        String changeName = myProfilePage.getLastNameInput().getAttribute("value");
-//        Assert.assertEquals(lastName, "Smith");
-//        Assert.assertNotEquals(lastName, "Kowalska");
-//        Assert.assertEquals(lastName, changeName);
-//    }
-//
-//    @Test
-//    public void userShouldChangeCountry() {
-//        //TODO create assertions -confirm country name and get correct string message
-//        myProfilePage.selectCountry("Brazil");
-//        myProfilePage.submitMyProfileUpdate();
-//    }
+
+    @Test
+    public void userShouldNotBeAllowedToChangeFirstNameField() {
+        assertThat(myProfilePage.isFirstNameReadOnly())
+                .as("First name field is read-only")
+                .isTrue();
+    }
+
+    @Test
+    public void userShouldNotBeAllowedToChangeLastNameField() {
+        assertThat(myProfilePage.isLastNameReadOnly())
+                .as("Last name field is read-only")
+                .isTrue();
+    }
+
+    @Test
+    public void userCouldChangeAddressAndPhoneNumber(){
+//        myProfilePage.fillPhonenumber()
+        navbarHeader.logOut();
+        assertThat(myProfilePage.getCurrentUrl())
+                .isEqualTo(loginPage.getUrl());
+    }
+
 
     //TODO create more options tests
 
